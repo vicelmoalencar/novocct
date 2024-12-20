f1ce5fdc41540105238e09f8f28bb2916354d7ee
Seu Nome <seu-email@exemplo.com>
2024-12-20 19:23:01 -0300

    Atualiza host do banco de dados para cct-db

--- cct/settings.py
+++ cct/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'cct'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'HOST': os.getenv('POSTGRES_HOST', 'cct-db'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
