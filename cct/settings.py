48b6746c8860a6da6112d37355b1e69f1038d749
Seu Nome <seu-email@exemplo.com>
2024-12-20 19:29:39 -0300

    Volta para PostgreSQL com configurações corretas

--- cct/settings.py
+++ cct/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = "cct.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'cct'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'qaz123wsx'),
+        'HOST': os.getenv('POSTGRES_HOST', 'cct-db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
