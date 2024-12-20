51aa1af0a13ed1fefaad71d0a5ebce08e6d90292
Seu Nome <seu-email@exemplo.com>
2024-12-20 19:27:13 -0300

    Muda temporariamente para SQLite para migrações iniciais

--- cct/settings.py
+++ cct/settings.py
@@ -96,12 +96,8 @@ WSGI_APPLICATION = "cct.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'cct'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'cct-db'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
